91386c2124f6ea35aad461800e8ad7df571b050d
DeividasRo <slenderiss@gmail.com>
2022-09-19 00:06:14 +0300

    Prepared main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+std::string hash(std::string input)
+{
+    std::string output;
+
+    return output;
+}
+
+int main()
+{
+    std::string input, output;
+    hash(input);
+    return 0;
+}
